7de4197509334623f3abbaa6e970d35ba6e2faf1
Mason McCoy <mmasonmccoy@gmail.com>
2022-11-16 19:54:34 -0600

    Donation buttons now post donation values to the db.

--- client/src/Buttons.js
+++ client/src/Buttons.js
@@ -2,20 +2,31 @@ import React from 'react'
 
 export default function Buttons({ setTotal }) {
 
-    function handleDonateClick(donation) {
+    function handleDonateClick(bttnVal) {
+        const value = bttnVal.donation;
         setTotal(prevTotal => {
-            const donationTotal = prevTotal + donation
+            const donationTotal = prevTotal + value
             return donationTotal;
         })
+
+        fetch("http://localhost:3001/users", {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(bttnVal)
+        })
+        .then((data) => console.log(data));
     }
 
     return (
         <>
-        <button onClick={() => handleDonateClick(5)}>$5</button>
-        <button onClick={() => handleDonateClick(10)}>$10</button>
-        <button onClick={() => handleDonateClick(100)}>$100</button>
-        <button onClick={() => handleDonateClick(1000)}>$1,000</button>
-        <button onClick={() => handleDonateClick(10000)}>$10,000</button>
+        <button onClick={() => handleDonateClick({donation: 5})}>$5</button>
+        <button onClick={() => handleDonateClick({donation: 10})}>$10</button>
+        <button onClick={() => handleDonateClick({donation: 100})}>$100</button>
+        <button onClick={() => handleDonateClick({donation: 1000})}>$1,000</button>
+        <button onClick={() => handleDonateClick({donation: 10000})}>$10,000</button>
         </>
     )
 }

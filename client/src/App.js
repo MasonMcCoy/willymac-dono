47d21208722fbd6d18906b144048a02530e50533
Mason McCoy <mmasonmccoy@gmail.com>
2022-11-16 19:05:43 -0600

    Displayed total is now set from sum of users donations on page load.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './Header';
 import Counter from './Counter';
 import Buttons from './Buttons';
@@ -7,6 +7,16 @@ export default function App() {
   // This will initialize with what we have in the db
   const [total, setTotal] = useState(0);
   
+  useEffect(() => {
+    fetch("http://localhost:3001/users")
+    .then((response) => response.json())
+    .then((data) => {
+      const userDonos = data.map(user => user.donation);
+      setTotal(userDonos.reduce((acc, curr) => acc + curr));
+      }
+    )
+  }, []);
+
   return (
     <>
       <Header />
